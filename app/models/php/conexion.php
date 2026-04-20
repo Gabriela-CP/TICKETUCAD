80c6c0160f14b28c90e0ce6ede496d0d71147347
zshoner <flamencijonathan@gmail.com>
2026-04-20 15:01:20 -0600

    LA BASE DE DATOS Clever Cloud ESTA CONECTADA

--- app/models/php/conexion.php
+++ app/models/php/conexion.php
@@ -1,16 +1,20 @@
 <?php
-// Credenciales por defecto de XAMPP
-$host = 'localhost';
-$dbname = 'ucad_tiket';
-$username = 'root'; // Usuario por defecto en XAMPP
-$password = '';     // Contraseña por defecto en XAMPP (suele estar vacía)
+// Credenciales de la base de datos remota (Clever Cloud)
+$host = 'brb6t1smbpzguvmk73ch-mysql.services.clever-cloud.com';
+$port = '3306';
+$dbname = 'brb6t1smbpzguvmk73ch';
+$username = 'ubs3smfytw84xd9c'; 
+$password = '6XytXkRqcFF7f3GiGXZw'; 
 
 try {
-    // Intentamos establecer la conexión
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Intentamos establecer la conexión incluyendo "port=$port;"
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     
     // Configuramos PDO para que nos muestre los errores si algo falla
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Puedes descomentar la siguiente línea para probar que funciona:
+    // echo "Conectado exitosamente"; 
     
 } catch(PDOException $e) {
     // Si hay un error, detenemos el proceso y mostramos qué pasó
